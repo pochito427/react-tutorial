70d7b899e9e8291b91914b3d9feed1d857a83f05
Alfonso Jimenez <djalfo18@gmail.com>
2019-03-25 20:22:15 -0500

    Updates

---
+++ precache-manifest.61e6b9956b0ebc58d30af92007550778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "32b0eb3363428e6c7453",
+    "url": "/react-tutorial/static/js/main.c93a8268.chunk.js"
+  },
+  {
+    "revision": "2822e1e6695fbf0ae41b",
+    "url": "/react-tutorial/static/js/2.44b20be8.chunk.js"
+  },
+  {
+    "revision": "32b0eb3363428e6c7453",
+    "url": "/react-tutorial/static/css/main.22ab6931.chunk.css"
+  },
+  {
+    "revision": "b0a36c2b72ef9d93ea4220b6c6784c8f",
+    "url": "/react-tutorial/index.html"
+  }
+];
